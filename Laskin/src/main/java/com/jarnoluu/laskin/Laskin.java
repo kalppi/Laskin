3c3fdd9fc50b38353775826e24223046ee136339
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2017-02-24 23:26:29 +0200

    update

--- Laskin/src/main/java/com/jarnoluu/laskin/Laskin.java
+++ Laskin/src/main/java/com/jarnoluu/laskin/Laskin.java
@@ -3,13 +3,13 @@ package com.jarnoluu.laskin;
 import com.jarnoluu.laskin.ui.GUI;
 import com.jarnoluu.laskin.ui.TextUI;
 import com.jarnoluu.laskin.ui.UIManager;
-
+        
 /**
  *
  * @author Jarno Luukkonen <luukkonen.jarno@gmail.com>
  */
 public class Laskin {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {        
         UIManager.addUI(new GUI());
     }
 }
